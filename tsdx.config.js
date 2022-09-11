d4b6553be6130bc6b199b0c14bad08fa1cfaedef
Soso Soso <soso.prso@gmail.com>
2022-09-11 22:36:16 +0200

    fix(postcss): export file

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,12 +1,16 @@
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const tailwindcss = require('tailwindcss');
+const path = require('path');
+
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
       postcss({
         plugins: [
           autoprefixer(),
+          tailwindcss(),
           cssnano({
             preset: 'default',
           }),
@@ -14,6 +18,9 @@ module.exports = {
         inject: false,
         // only write out CSS for the first bundle (avoids pointless extra files):
         extract: !!options.writeMeta,
+        extract: true,
+        // Or with custom file name
+        extract: path.resolve('dist/styles.css'),
       })
     );
     return config;
