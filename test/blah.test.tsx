5a5523060375cc20d7003a1e3e5fb5c846326ccc
Soso Soso <soso.prso@gmail.com>
2022-09-11 17:05:25 +0200

    remove default github workflows

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Button } from '../src/components/atoms/Button/Button';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Button label="ok" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
